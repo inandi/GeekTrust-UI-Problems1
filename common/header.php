c3efab78955960924d67ddc8041adbcc094ebf75
gobinda.genx@yahoo.in <gobinda.genx@yahoo.in>
2020-05-16 12:54:15 +0530

    bugfix

--- common/header.php
+++ common/header.php
@@ -1,6 +1,7 @@
 <div class="header">
    <span class="pull-right">
       <a class="reset-search-form" href="javascript:resetForm();" style="display: none;margin-right: 10px;"> Reset </a>
+      <span class="reset-search-form" style="display: none;margin-right: 10px;">|</span>
       <a href="<?php echo BASE_URL; ?>"> GeekTrust Home </a>
    </span>
    <h1>Finding Falcone!</h1>
